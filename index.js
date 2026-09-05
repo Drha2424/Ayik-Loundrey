435d5a4d040cb68320eecbf5ccfedaed2467781e
Drha2424 <dwikirichi@gmail.com>
2026-09-05 21:21:13 +0800

    kntll

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 import express from 'express';
 import pinoHttp from 'pino-http';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import 'dotenv/config';
 
 import logger from './backend/configs/logger.config.js';
 import router from './backend/routes/index.route.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,18 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Sajikan file statis (HTML, CSS, JS frontend)
-app.use(express.static(path.join(__dirname, 'tampilan-public')));
-app.use('/css', express.static(path.join(__dirname, 'css')));
-app.use('/js', express.static(path.join(__dirname, 'js')));
-
-// Routing API
+// Express HANYA melayani API
 app.use('/api', router);
-
-// Halaman utama (root) menampilkan index.html dari tampilan-public
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'tampilan-public', 'index.html'));
-});
 
 // Error Handler
 app.use((err, req, res, next) => {
